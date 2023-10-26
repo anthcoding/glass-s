58f2f105d74836fa4dc6347844dd3ed093e9781c
Anth Lorenzo <anthlorenzo@Anths-Air.attlocal.net>
2023-10-26 00:00:55 -0400

    Routes api error 2

--- app/api/route.js
+++ app/api/route.js
@@ -26,32 +26,29 @@ const generateEmailContent = (data) => {
 	};
 };
 
-export const handler = async (req, res) => {
-	if (req.method === 'POST') {
-		const data = req.body;
-		if (
-			!data ||
-			!data.name ||
-			!data.lastName ||
-			!data.email ||
-			!data.subject ||
-			!data.message
-		) {
-			return res.status(400).send({ message: 'Bad request' });
-		}
-
-		try {
-			await transporter.sendMail({
-				...mailOptions,
-				...generateEmailContent(data),
-				subject: data.subject,
-			});
-
-			return res.status(200).json({ success: true });
-		} catch (err) {
-			console.log(err);
-			return res.status(400).json({ message: err.message });
-		}
+export async function POST(req, res) {
+	const data = req.body;
+	if (
+		!data ||
+		!data.name ||
+		!data.lastName ||
+		!data.email ||
+		!data.subject ||
+		!data.message
+	) {
+		return res.status(400).send({ message: 'Bad request' });
 	}
-	return res.status(400).json({ message: 'Bad request' });
-};
+
+	try {
+		await transporter.sendMail({
+			...mailOptions,
+			...generateEmailContent(data),
+			subject: data.subject,
+		});
+
+		return res.status(200).json({ success: true });
+	} catch (err) {
+		console.log(err);
+		return res.status(400).json({ message: err.message });
+	}
+}
